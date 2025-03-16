6383e0ac6a016964a5e33b7d04c5f115709902ab
Pedro Henrique <phas050902@gmail.com>
2025-03-16 18:19:46 -0300

    feat: implementing tests

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -3,30 +3,96 @@ package br.ce.wcaquino.servicos;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 import org.hamcrest.CoreMatchers;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import org.junit.rules.ExpectedException;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.exceptions.FilmeSemEstoqueException;
+import br.ce.wcaquino.exceptions.LocadoraException;
 import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoServiceTest {
+	
+	private	LocacaoService locacaoService;
+	
+	@Rule
+	public ErrorCollector error = new ErrorCollector();
+	
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
 
+	@Before
+	public void setup() {
+		locacaoService = new LocacaoService();
+	}
+
+	
 	@Test
-	public void teste(){
-		LocacaoService locacaoService = new LocacaoService();
+	public void testLocacao() throws Exception{
+		//Cenario
 		Usuario usuario = new Usuario("Pedro");
-		Filme filme = new Filme("Vingadores Ultimato", 1, 10.0);
+		List<Filme> filmes = Arrays.asList(new Filme("Vingadores Ultimato", 1, 10.0));
+				
+		//Acao
+		Locacao locacao = locacaoService.alugarFilme(usuario, filmes);
 		
-		Locacao locacao = locacaoService.alugarFilme(usuario, filme);
-			
 		//Testes
-		assertThat(locacao.getValor(), CoreMatchers.is(10.0));
-		assertThat(locacao.getValor(), CoreMatchers.is(CoreMatchers.not(12.0)));
-		assertThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), CoreMatchers.is(true));
-		assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+		error.checkThat(locacao.getValor(), CoreMatchers.is(10.0));
+		error.checkThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), CoreMatchers.is(true));
+		error.checkThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), CoreMatchers.is(true));
+	}
+	
+	//Elegante
+	@Test(expected = FilmeSemEstoqueException.class)
+	public void testLocacao_filmeSemEstoque() throws Exception {
+		//Cenario
+		Usuario usuario = new Usuario("Pedro");
+		List<Filme> filmes = Arrays.asList(new Filme("Vingadores Ultimato", 0, 10.0));
+		
+		//Acao
+		locacaoService.alugarFilme(usuario, filmes);
+	}
+	
+	//Robusta
+	@Test
+	public void testLocacao_usuarioVazio() throws FilmeSemEstoqueException {
+		//cenario
+		List<Filme> filmes = Arrays.asList(new Filme("Vingadores Ultimato", 1, 10.0));
+
+		//acao
+		try {
+			locacaoService.alugarFilme(null, filmes);
+			Assert.fail();
+		} catch (LocadoraException e) {
+			Assert.assertThat(e.getMessage(), CoreMatchers.is("Usuario vazio"));
+		}
+	}
+	
+	//Atual
+	@Test
+	public void testLocacao_filmeVazio() throws LocadoraException, FilmeSemEstoqueException {
+		//cenario
+		Usuario usuario = new Usuario("Pedro");
+
+		exception.expect(LocadoraException.class);
+		exception.expectMessage("Filme vazio");
+		
+		//acao
+		locacaoService.alugarFilme(usuario, null);
 	}
 }
